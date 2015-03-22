a59c3b87e49ac15c9f81b0dd33543827343bf32f
Wojtek Dudzik <wojtek.adam.dudzik@gmail.com>
2015-03-22 14:27:09 +0100

    Added first version of project

---
+++ Tower_Defense/main.cpp
@@ -0,0 +1,255 @@
+#include <stdlib.h>
+#include <gl/glut.h>
+#include <iostream>
+
+#include <glm/glm.hpp>					//biblioteka pomocnicza zawierajaca operacje na macierzach i wektorach
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+GLfloat anglePyramid = 0.0f;  // Rotational angle for pyramid 
+GLfloat angleCube = 0.0f;     // Rotational angle for cube
+int refreshMills = 15;        // refresh interval in milliseconds
+
+void DrawBox();
+
+// wpó³rzêdne po³o¿enia obserwatora
+GLdouble eyey = 3;
+
+
+// wspó³rzêdne punktu w którego kierunku jest zwrócony obserwator,
+GLdouble centerx = 0;
+GLdouble centery = 0;
+GLdouble centerz = 0;
+
+GLfloat kat_obrotu = 0;
+
+void initGL() {
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set background color to black and opaque
+	glClearDepth(1.0f);                   // Set background depth to farthest
+	glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+	glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+	glShadeModel(GL_SMOOTH);   // Enable smooth shading
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+}
+
+// funkcja generuj¹ca scenê 3D
+void Display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+
+	glMatrixMode(GL_MODELVIEW);  
+	glLoadIdentity();
+
+	float x2 = sin(glm::radians(kat_obrotu)) * 5;		//tutaj powinno byæ 5.19615  (sqrt(27))
+	float z2 = cos(glm::radians(kat_obrotu)) * 5;
+	gluLookAt( x2 + centerx, eyey, z2 + centerz, centerx, centery, centerz, 0, 1, 0 );	//prosta kamera która obraca siê  i przesuwa w jednej p³aszczyŸnie (xz) 
+
+
+	glRotatef(angleCube, 0.0f, 0.0f, 1.0f); 
+	DrawBox();
+	glRotatef(angleCube, 0.0f, 0.0f, -1.0f); 
+
+	
+	glTranslatef(-1.5f, 0.0f, -6.0f);			//przesuniecie do punktu
+	glRotatef(anglePyramid, 1.0f, 0.0f, 0.0f);	//obrot wokó³ w³asnej osi 
+
+	glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
+	// Front
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f( 0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(1.0f, -1.0f, 1.0f);
+
+	// Right
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	// Back
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+
+	// Left
+	glColor3f(1.0f,0.0f,0.0f);       // Red
+	glVertex3f( 0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f,0.0f,1.0f);       // Blue
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glColor3f(0.0f,1.0f,0.0f);       // Green
+	glVertex3f(-1.0f,-1.0f, 1.0f);
+	glEnd();   // Done drawing the pyramid
+
+	glRotatef(anglePyramid, -1.0f, 0.0f, 0.0f);	//odtworzenie pierwotnego stanu macierzy
+	glTranslatef(1.5f, 0.0f, 6.0f); 
+	
+	glFlush();
+
+	glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+
+	anglePyramid += 0.9f;
+	angleCube -= 0.9f;
+}
+
+void DrawBox()
+{
+	glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+	// Top face (y = 1.0f)
+	// Define vertices in counter-clockwise (CCW) order with normal pointing out
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f( 1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f,  1.0f);
+	glVertex3f( 1.0f, 1.0f,  1.0f);
+
+	// Bottom face (y = -1.0f)
+	glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+	glVertex3f( 1.0f, -1.0f,  1.0f);
+	glVertex3f(-1.0f, -1.0f,  1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f( 1.0f, -1.0f, -1.0f);
+
+	// Front face  (z = 1.0f)
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f( 1.0f,  1.0f, 1.0f);
+	glVertex3f(-1.0f,  1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f( 1.0f, -1.0f, 1.0f);
+
+	// Back face (z = -1.0f)
+	glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+	glVertex3f( 1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f,  1.0f, -1.0f);
+	glVertex3f( 1.0f,  1.0f, -1.0f);
+
+	// Left face (x = -1.0f)
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(-1.0f,  1.0f,  1.0f);
+	glVertex3f(-1.0f,  1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f,  1.0f);
+
+	// Right face (x = 1.0f)
+	glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+	glVertex3f(1.0f,  1.0f, -1.0f);
+	glVertex3f(1.0f,  1.0f,  1.0f);
+	glVertex3f(1.0f, -1.0f,  1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glEnd();  // End of drawing color-cube
+}
+
+// zmiana wielkoœci okna
+void Reshape( int width, int height )
+{
+	// Compute aspect ratio of the new window
+	if (height == 0) height = 1;                // To prevent divide by 0
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+	// Set the viewport to cover the new window
+	glViewport(0, 0, width, height);
+
+	// Set the aspect ratio of the clipping volume to match the viewport
+	glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+	glLoadIdentity();             // Reset
+	// Enable perspective projection with fovy, aspect, zNear and zFar
+	gluPerspective(90.0f, aspect, 0.1f, 100.0f);		//kat widzenia, aspect ratio, zNear, zFar
+}
+
+
+void timer(int value) {
+	glutPostRedisplay();      // Post re-paint request to activate display()
+	glutTimerFunc(refreshMills, timer, 0); // next timer call milliseconds later
+}
+
+
+void Keyboard( unsigned char key, int x, int y )
+{
+	if( key == 'z')
+		kat_obrotu += 5;
+
+	if( key == 'x')
+		kat_obrotu -= 5;
+
+	// odrysowanie okna
+	Reshape( glutGet( GLUT_WINDOW_WIDTH ), glutGet( GLUT_WINDOW_HEIGHT ) );
+}
+
+//Dzia³a nie tykaæ
+void SpecialKeys( int key, int x, int y )
+{
+	switch( key )
+	{
+		// kursor w lewo
+	case GLUT_KEY_LEFT:
+		centerx -= 0.5 * cos(glm::radians(kat_obrotu));
+		centerz += 0.5 * sin(glm::radians(kat_obrotu));
+		break;
+
+		// kursor w górê
+	case GLUT_KEY_UP:
+		centerz -= 0.5 * cos(glm::radians(kat_obrotu));
+		centerx -= 0.5 * sin(glm::radians(kat_obrotu));
+		break;
+
+		// kursor w prawo
+	case GLUT_KEY_RIGHT:
+		centerx += 0.5 * cos(glm::radians(kat_obrotu));
+		centerz -= 0.5 * sin(glm::radians(kat_obrotu));
+		break;
+
+		// kursor w dó³
+	case GLUT_KEY_DOWN:
+		centerz += 0.5 * cos(glm::radians(kat_obrotu));
+		centerx += 0.5 * sin(glm::radians(kat_obrotu));
+		break;
+	}
+	// odrysowanie okna
+	Reshape( glutGet( GLUT_WINDOW_WIDTH ), glutGet( GLUT_WINDOW_HEIGHT ) );
+}
+
+
+
+
+int main( int argc, char * argv[] )
+{
+	// inicjalizacja biblioteki GLUT
+	glutInit( & argc, argv );
+
+	// inicjalizacja bufora ramki
+	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA );
+
+	// rozmiary g³ównego okna programu
+	glutInitWindowSize( 640, 480 );
+
+	// utworzenie g³ównego okna programu
+	glutCreateWindow( "Tower Defense" );
+
+	// do³¹czenie funkcji generuj¹cej scenê 3D
+	glutDisplayFunc( Display );
+
+	// do³¹czenie funkcji wywo³ywanej przy zmianie rozmiaru okna
+	glutReshapeFunc( Reshape );
+
+	// do³¹czenie funkcji obs³ugi klawiatury
+	glutKeyboardFunc( Keyboard );
+
+	// do³¹czenie funkcji obs³ugi klawiszy funkcyjnych i klawiszy kursora
+	glutSpecialFunc( SpecialKeys );
+
+	initGL(); 
+	glutTimerFunc(0, timer, 0);
+
+	// wprowadzenie programu do obs³ugi pêtli komunikatów
+	glutMainLoop();
+
+	return 0;
+}
