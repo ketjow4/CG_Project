85031559d49dd55d6e3eb61f23e884a79c360def
Wojtek Dudzik <wojtek.adam.dudzik@gmail.com>
2015-05-13 16:10:45 +0200

    Level class added, still hardcoded

--- Tower_Defense/Level.cpp
+++ Tower_Defense/Level.cpp
@@ -6,33 +6,44 @@ Level::Level()
 
 Level::~Level()
 {
+	delete wave;
+	delete path;
+	delete terrain;
 }
 
 void Level::Load()
 {
-	list<Enemy*> enList;
 
+
+	terrain = new Terrain();
+	terrain->Init("Models/terrain1.bmp", 0.3f);
+
+	path = new Path();
+	path->Init("Models/path1.bmp");
+
+	list<Enemy*> *enList;		//remember to add destructor
+
+	enList = new list<Enemy*>();
 	Enemy *en = new Enemy();
 	en->light = Engine::getEngine().getLight();
 	en->LoadModel(1, "Models/phoenix_ugv.md2");
 	en->terrain = terrain;
 	en->path = path;
-	enList.push_back(en);
+	enList->push_back(en);
 	
 	en = new Enemy();
 	en->light = Engine::getEngine().getLight();
 	en->LoadModel(1, "Models/phoenix_ugv.md2");
 	en->terrain = terrain;
 	en->path = path;
-	enList.push_back(en);
+	enList->push_back(en);
 
 	en = new Enemy();
 	en->light = Engine::getEngine().getLight();
 	en->LoadModel(1, "Models/phoenix_ugv.md2");
 	en->terrain = terrain;
 	en->path = path;
-	enList.push_back(en);
+	enList->push_back(en);
 
-	wave = new Wave(&enList, 0, 50);
-
+	wave = new Wave(enList, 0, 50);
 }
