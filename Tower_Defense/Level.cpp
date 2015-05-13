b5d1f307bad86bc7d7edce86557e3d393c3da89a
TheVeryRandomGitUser <adie.lipinski@gmail.com>
2015-05-13 16:13:46 +0200

    Merged pt2

--- Tower_Defense/Level.cpp
+++ Tower_Defense/Level.cpp
@@ -14,21 +14,21 @@ void Level::Load()
 
 	Enemy *en = new Enemy();
 	en->light = Engine::getEngine().getLight();
-	en->LoadModel(1, "Models/phoenix_ugv.md2");
+	en->LoadModel(1);
 	en->terrain = terrain;
 	en->path = path;
 	enList.push_back(en);
 	
 	en = new Enemy();
 	en->light = Engine::getEngine().getLight();
-	en->LoadModel(1, "Models/phoenix_ugv.md2");
+	en->LoadModel(1);
 	en->terrain = terrain;
 	en->path = path;
 	enList.push_back(en);
 
 	en = new Enemy();
 	en->light = Engine::getEngine().getLight();
-	en->LoadModel(1, "Models/phoenix_ugv.md2");
+	en->LoadModel(1);
 	en->terrain = terrain;
 	en->path = path;
 	enList.push_back(en);
