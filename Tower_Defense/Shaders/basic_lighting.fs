f9e709fcb2b6441831c3a92d6a01a78f73e9379c
Wojtek Dudzik <wojtek.adam.dudzik@gmail.com>
2015-05-19 21:33:10 +0200

    Added new shaders

--- Tower_Defense/Shaders/basic_lighting.fs
+++ Tower_Defense/Shaders/basic_lighting.fs
@@ -3,6 +3,7 @@
 const int MAX_POINT_LIGHTS = 10;   
 const int MAX_SPOT_LIGHTS = 2;   
 
+in vec4 LightSpacePos;  
 in vec2 TexCoord0;
 in vec3 Normal0;     
 in vec3 WorldPos0;                                                                 
@@ -55,14 +56,30 @@ uniform DirectionalLight gDirectionalLight;
 uniform PointLight gPointLights[MAX_POINT_LIGHTS];  
 uniform SpotLight gSpotLights[MAX_SPOT_LIGHTS];                                          
 uniform sampler2D gSampler;   
+uniform sampler2D gShadowMap;  
 uniform vec3 gEyeWorldPos;                                                          
 uniform float gMatSpecularIntensity;                                                
 uniform float gSpecularPower;  
 
 uniform ColorEffect gColorEffect;                                                     
 
+float CalcShadowFactor(vec4 LightSpacePos)                                                  
+{                                                                                           
+    vec3 ProjCoords = LightSpacePos.xyz / LightSpacePos.w;                                  
+    vec2 UVCoords;                                                                          
+    UVCoords.x = 0.5 * ProjCoords.x + 0.5;                                                  
+    UVCoords.y = 0.5 * ProjCoords.y + 0.5;                                                  
+    float z = 0.5 * ProjCoords.z + 0.5;                                                     
+    float Depth = texture(gShadowMap, UVCoords).x;                                          
+    if (Depth < z + 0.00001)                                                                 
+        return 0.0;                                                                         
+    else                                                                                    
+        return 1.0;                                                                         
+}      
 
-vec4 CalcLightInternal(BaseLight Light, vec3 LightDirection, vec3 Normal)                   
+
+vec4 CalcLightInternal(BaseLight Light, vec3 LightDirection, vec3 Normal,            
+                       float ShadowFactor)                                                  
 {                                                                                           
     vec4 AmbientColor = vec4(Light.Color, 1.0f) * Light.AmbientIntensity;                   
     float DiffuseFactor = dot(Normal, -LightDirection);                                     
@@ -83,41 +100,42 @@ vec4 CalcLightInternal(BaseLight Light, vec3 LightDirection, vec3 Normal)
         }                                                                                   
     }                                                                                       
                                                                                             
-    return (AmbientColor + DiffuseColor + SpecularColor);                                   
-} 
+    return (AmbientColor + ShadowFactor * (DiffuseColor + SpecularColor));                  
+}  
 
 vec4 CalcDirectionalLight(vec3 Normal)                                                      
-{                                                                                           
-    return CalcLightInternal(gDirectionalLight.Base, gDirectionalLight.Direction, Normal); 
-}
+{                                                                                              
+    return CalcLightInternal(gDirectionalLight.Base, gDirectionalLight.Direction, Normal, 1.0);  
+}      
 
-vec4 CalcPointLight(PointLight l, vec3 Normal)                                              
+vec4 CalcPointLight(PointLight l, vec3 Normal, vec4 LightSpacePos)                   
 {                                                                                           
     vec3 LightDirection = WorldPos0 - l.Position;                                           
     float Distance = length(LightDirection);                                                
     LightDirection = normalize(LightDirection);                                             
+    float ShadowFactor = CalcShadowFactor(LightSpacePos);                                   
                                                                                             
-    vec4 Color = CalcLightInternal(l.Base, LightDirection, Normal);                         
+    vec4 Color = CalcLightInternal(l.Base, LightDirection, Normal, ShadowFactor);           
     float Attenuation =  l.Atten.Constant +                                                 
                          l.Atten.Linear * Distance +                                        
                          l.Atten.Exp * Distance * Distance;                                 
                                                                                             
     return Color / Attenuation;                                                             
-}    
+}       
 
-vec4 CalcSpotLight(SpotLight l, vec3 Normal)                                                
+vec4 CalcSpotLight(SpotLight l, vec3 Normal, vec4 LightSpacePos)                     
 {                                                                                           
     vec3 LightToPixel = normalize(WorldPos0 - l.Base.Position);                             
     float SpotFactor = dot(LightToPixel, l.Direction);                                      
                                                                                             
     if (SpotFactor > l.Cutoff) {                                                            
-        vec4 Color = CalcPointLight(l.Base, Normal);                                        
+        vec4 Color = CalcPointLight(l.Base, Normal, LightSpacePos);                         
         return Color * (1.0 - (1.0 - SpotFactor) * 1.0/(1.0 - l.Cutoff));                   
     }                                                                                       
     else {                                                                                  
         return vec4(0,0,0,0);                                                               
     }                                                                                       
-}         
+}        
 
 void main()                                                                         
 {                                                                                   
@@ -125,14 +143,17 @@ void main()
     vec4 TotalLight = CalcDirectionalLight(Normal);                                         
 
     for (int i = 0 ; i < gNumPointLights ; i++) {                                           
-        TotalLight += CalcPointLight(gPointLights[i], Normal);                              
-    }   
-
-	for (int i = 0 ; i < gNumSpotLights ; i++)
-	{                                            
-        TotalLight += CalcSpotLight(gSpotLights[i], Normal);                                
-    }     
+        TotalLight += CalcPointLight(gPointLights[i], Normal, LightSpacePos);               
+    }                                                                                       
+                                                                                            
+    for (int i = 0 ; i < gNumSpotLights ; i++) {                                            
+        TotalLight += CalcSpotLight(gSpotLights[i], Normal, LightSpacePos);                 
+    }      
 	
-    vec4 finalColor = texture2D(gSampler, TexCoord0.xy) * TotalLight; 
-	FragColor = mix(finalColor, gColorEffect.Color, gColorEffect.EffectIntensity);
+    //vec4 finalColor = texture2D(gSampler, TexCoord0.xy) * TotalLight; 
+	//FragColor = mix(finalColor, gColorEffect.Color, gColorEffect.EffectIntensity);
+	//FragColor = finalColor;
+	
+	vec4 SampledColor = texture2D(gSampler, TexCoord0.xy);                                  
+    FragColor = SampledColor * TotalLight;
 }
